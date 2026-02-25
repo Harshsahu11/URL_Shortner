7d17c3df5cb5f280bdf3ad3880536c6b4ff887a9
Harsh Sahu <harshsahu1109@gmail.com>
2026-02-25 21:06:42 +0530

    Router Dev

---
+++ controllers/url.controller.js
@@ -0,0 +1,23 @@
+const {nanoid} = require('nanoid');
+const URL = require("../models/url.model");
+
+async function handleGenerateNewShortId(req,res){
+    const body = req.body;
+    if(!body.url) {
+        return res.status(400).json({
+            message:"URL is required"
+        })
+    }
+    const shortID = nanoid(8);
+    await URL.create({
+        shortId : shortID,
+        redirectURL : body.url,
+        visitHistory:[],
+    });
+
+    return res.json({id:shortID});
+}
+
+module.exports = {
+    handleGenerateNewShortId
+}
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require("express");
 const {ConnectDB} = require("./db/db");
 
+const urlRoute = require('./routes/url.router');
 
 
 const app = express();
 const PORT = 8001;
 ConnectDB();
+
+app.use("/",urlRoute);
 
 
 app.listen(PORT,()=>{
