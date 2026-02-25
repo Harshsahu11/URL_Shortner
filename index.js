f1f0dae032e9c023075b03bf4aaf10b53d9c508a
Harsh Sahu <harshsahu1109@gmail.com>
2026-02-25 20:45:14 +0530

    Server Setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-~
+require('dotenv').config();
+const express = require("express");
+const {ConnectDB} = require("./db/db");
+
+
+
+const app = express();
+const PORT = 8001;
+ConnectDB();
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on Port: ${PORT}`);
+});
